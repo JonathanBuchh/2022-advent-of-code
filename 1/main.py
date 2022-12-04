3edb297c398176bd6daa069f30f52fa47caf2612
Jonathan Buchholz <jonathan@buchh.org>
2022-12-03 22:43:59 -0500

    Start advent of code

---
+++ 1/main.py
@@ -0,0 +1,26 @@
+def main():
+    with open('input.txt') as f:
+        file = f.readlines()
+
+    calories = [0]
+    count = 0
+
+    for line in file:
+        if len(line.strip()) == 0:
+            count += 1
+            calories.append(0)
+        else:
+            calories[count] += int(line.strip())
+
+    print('Max calories: ' + str(max(calories)))
+
+    top_three_total = 0
+
+    for i in range(3):
+        top_three_total += max(calories)
+        calories.remove(max(calories))
+
+    print('Top three total: ' + str(top_three_total))
+
+if __name__ == "__main__":
+    main()
